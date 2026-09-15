26c87c96b9fb5b21cc3e3ca7c38e4f3cdf997f10
yunxiaohong <>
2026-09-15 09:29:49 +0800

    结构体修改

--- src/struct_test.c
+++ src/struct_test.c
@@ -4,10 +4,15 @@ struct student{
     int age;
     char sex[4];
 };
+void print_student(struct student s){
+    printf("姓名：%s\n", s.name);
+    printf("年龄：%d\n", s.age);
+    printf("性别：%s\n", s.sex);
+}
 int main(){
-    struct student s1 = {"张三", 20, "男"};
-    printf("姓名：%s\n", s1.name);
-    printf("年龄：%d\n", s1.age);
-    printf("性别：%s\n", s1.sex);
+    struct student s1 = {"张三", 20, "男"}, 
+    s2 = {"李四", 22, "女"};
+    print_student(s1);
+    print_student(s2);
     return 0;
 }
